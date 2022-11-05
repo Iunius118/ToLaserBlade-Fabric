6f291b4c57f7e908735ec1b377ba6834cf1a5872
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2022-11-06 01:12:21 +0900

    Fix LBSwordItemRenderer to use model that loaded by LaserBladeModelManager

--- src/main/java/com/github/iunius118/tolaserblade/client/renderer/item/LBSwordItemRenderer.java
+++ src/main/java/com/github/iunius118/tolaserblade/client/renderer/item/LBSwordItemRenderer.java
@@ -1,7 +1,8 @@
 package com.github.iunius118.tolaserblade.client.renderer.item;
 
 import com.github.iunius118.tolaserblade.api.client.model.LaserBladeModel;
-import com.github.iunius118.tolaserblade.client.model.laserblade.LaserBladeModelType0;
+import com.github.iunius118.tolaserblade.client.model.LaserBladeModelManager;
+import com.github.iunius118.tolaserblade.core.laserblade.LaserBlade;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -9,10 +10,14 @@ import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.world.item.ItemStack;
 
 public class LBSwordItemRenderer implements BuiltinItemRendererRegistry.DynamicItemRenderer {
-    public LaserBladeModel model = new LaserBladeModelType0();
-
     @Override
     public void render(ItemStack stack, ItemTransforms.TransformType mode, PoseStack matrices, MultiBufferSource vertexConsumers, int light, int overlay) {
+        int modelType = LaserBlade.visualOf(stack).getModelType();
+        LaserBladeModel model = LaserBladeModelManager.getModel(modelType);
+
+        if (model == null)
+            return;
+
         matrices.pushPose();
         model.render(stack, mode, matrices, vertexConsumers, light, overlay);
         matrices.popPose();
