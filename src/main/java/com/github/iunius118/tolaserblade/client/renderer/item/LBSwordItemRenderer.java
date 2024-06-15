e8cdccaba65dc247c7191ecfee378ee8115264f4
iunius118 <18502177+Iunius118@users.noreply.github.com>
2024-06-15 15:02:41 +0900

    Improve laser blade model

--- src/main/java/com/github/iunius118/tolaserblade/client/renderer/item/LBSwordItemRenderer.java
+++ src/main/java/com/github/iunius118/tolaserblade/client/renderer/item/LBSwordItemRenderer.java
@@ -1,7 +1,7 @@
 package com.github.iunius118.tolaserblade.client.renderer.item;
 
 import com.github.iunius118.tolaserblade.api.client.model.LaserBladeModel;
-import com.github.iunius118.tolaserblade.client.model.laserblade.LaserBladeModelType0;
+import com.github.iunius118.tolaserblade.client.model.laserblade.LaserBladeModelType2;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -9,7 +9,7 @@ import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.world.item.ItemStack;
 
 public class LBSwordItemRenderer implements BuiltinItemRendererRegistry.DynamicItemRenderer {
-    public LaserBladeModel model = new LaserBladeModelType0();
+    public LaserBladeModel model = new LaserBladeModelType2();
 
     @Override
     public void render(ItemStack stack, ItemTransforms.TransformType mode, PoseStack matrices, MultiBufferSource vertexConsumers, int light, int overlay) {
