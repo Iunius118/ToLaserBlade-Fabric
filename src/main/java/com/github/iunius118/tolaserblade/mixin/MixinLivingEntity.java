7789f5e5a59f41a0c0d5f262775aa2fe67efdd22
iunius118 <18502177+Iunius118@users.noreply.github.com>
2024-10-26 01:48:07 +0900

    [1.21.3] Fix mixin classes

--- src/main/java/com/github/iunius118/tolaserblade/mixin/MixinLivingEntity.java
+++ src/main/java/com/github/iunius118/tolaserblade/mixin/MixinLivingEntity.java
@@ -2,7 +2,6 @@ package com.github.iunius118.tolaserblade.mixin;
 
 import com.github.iunius118.tolaserblade.world.item.LBSwordItem;
 import com.github.iunius118.tolaserblade.world.item.LaserBladeItemUtil;
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
@@ -32,7 +31,7 @@ public class MixinLivingEntity {
             // Server side
             // When laser blade swung
             // Play sound of laser blade when it's swung
-            LaserBladeItemUtil.playSwingSound(level, livingEntity, itemStack.has(DataComponents.FIRE_RESISTANT));
+            LaserBladeItemUtil.playSwingSound(level, livingEntity, itemStack);
         }
     }
 }
--- src/main/java/com/github/iunius118/tolaserblade/mixin/MixinSmithingMenu.java
+++ src/main/java/com/github/iunius118/tolaserblade/mixin/MixinSmithingMenu.java
@@ -2,20 +2,33 @@ package com.github.iunius118.tolaserblade.mixin;
 
 import com.github.iunius118.tolaserblade.world.item.ModItems;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.ContainerLevelAccess;
-import net.minecraft.world.inventory.ItemCombinerMenu;
-import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.inventory.SmithingMenu;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.world.inventory.*;
+import net.minecraft.world.item.crafting.RecipeAccess;
+import net.minecraft.world.item.crafting.RecipePropertySet;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SmithingMenu.class)
 public abstract class MixinSmithingMenu extends ItemCombinerMenu {
-    private MixinSmithingMenu(@Nullable MenuType<?> menuType, int i, Inventory inventory, ContainerLevelAccess containerLevelAccess) {
-        super(menuType, i, inventory, containerLevelAccess);
+    private MixinSmithingMenu(int i, Inventory inventory, ContainerLevelAccess containerLevelAccess, Level level) {
+        super(MenuType.SMITHING, i, inventory, containerLevelAccess, createInputSlotDefinitions(level.recipeAccess()));
+    }
+
+    @Unique
+    private static ItemCombinerMenuSlotDefinition createInputSlotDefinitions(RecipeAccess recipeAccess) {
+        RecipePropertySet recipePropertySet = recipeAccess.propertySet(RecipePropertySet.SMITHING_BASE);
+        RecipePropertySet recipePropertySet2 = recipeAccess.propertySet(RecipePropertySet.SMITHING_TEMPLATE);
+        RecipePropertySet recipePropertySet3 = recipeAccess.propertySet(RecipePropertySet.SMITHING_ADDITION);
+        return ItemCombinerMenuSlotDefinition.create()
+                .withSlot(0, 8, 48, recipePropertySet2::test)
+                .withSlot(1, 26, 48, recipePropertySet::test)
+                .withSlot(2, 44, 48, recipePropertySet3::test)
+                .withResultSlot(3, 98, 48)
+                .build();
     }
 
     @Inject(method = "shrinkStackInSlot(I)V", at = @At("HEAD"), cancellable = true)
