8eaf5ea91888be195a58ff33a2b5107e0af2141b
iunius118 <18502177+Iunius118@users.noreply.github.com>
2024-12-23 00:51:59 +0900

    Remove `laserBlades` from config

--- src/main/java/com/github/iunius118/tolaserblade/integration/autoconfig/ModConfig.java
+++ src/main/java/com/github/iunius118/tolaserblade/integration/autoconfig/ModConfig.java
@@ -3,22 +3,12 @@ package com.github.iunius118.tolaserblade.integration.autoconfig;
 import com.github.iunius118.tolaserblade.ToLaserBlade;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
-import net.minecraft.ResourceLocationException;
-import net.minecraft.core.Holder;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 @Config(name = ToLaserBlade.MOD_ID)
 public class ModConfig implements ConfigData {
     boolean enableLaserTrap = true;
     boolean canLaserTrapAttackPlayer = true;
     boolean canLaserTrapHeatUpFurnace = true;
-    ClientConfig client = new ClientConfig();
 
     public boolean isLaserTrapEnabled() {
         return enableLaserTrap;
@@ -30,35 +20,5 @@ public class ModConfig implements ConfigData {
 
     public boolean canLaserTrapHeatUpFurnace() {
         return canLaserTrapHeatUpFurnace;
-    }
-
-    public List<Item> getLaserBladeItems() {
-        return getLaserBladeItemIDs().stream()
-                .map(BuiltInRegistries.ITEM::get)
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .map(Holder.Reference::value)
-                .toList();
-    }
-
-    public List<ResourceLocation> getLaserBladeItemIDs(){
-        return Stream.of(client.laserBlades)
-                .map(this::getResourceLocation)
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .filter(BuiltInRegistries.ITEM::containsKey)
-                .toList();
-    }
-
-    private Optional<ResourceLocation> getResourceLocation(String string){
-        try {
-            return Optional.ofNullable(ResourceLocation.tryParse(string));
-        } catch (ResourceLocationException e) {
-            return Optional.empty();
-        }
-    }
-
-    static class ClientConfig {
-        String[] laserBlades = {"tolaserblade:laser_blade", "tolaserblade:laser_blade_fp"};
     }
 }
