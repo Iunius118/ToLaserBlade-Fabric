86ac524fa75c6c8bc0f323736071e8f589f38c4c
iunius118 <18502177+Iunius118@users.noreply.github.com>
2024-12-23 00:51:03 +0900

    [1.21.4] Remove item models

--- src/main/java/com/github/iunius118/tolaserblade/client/renderer/item/model/LaserBladeModelLoadingPlugin.java
+++ src/main/java/com/github/iunius118/tolaserblade/client/renderer/item/model/LaserBladeModelLoadingPlugin.java
@@ -1,58 +1,12 @@
 package com.github.iunius118.tolaserblade.client.renderer.item.model;
 
 import com.github.iunius118.tolaserblade.client.model.LaserBladeModelManager;
-import com.github.iunius118.tolaserblade.integration.autoconfig.ModConfig;
-import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
-import net.fabricmc.fabric.api.client.model.loading.v1.ModelModifier;
-import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.client.resources.model.UnbakedModel;
-import net.minecraft.resources.ResourceLocation;
-
-import java.util.List;
 
 public class LaserBladeModelLoadingPlugin implements ModelLoadingPlugin {
-    private static List<ModelResourceLocation> laserBladeModelLocations = List.of();
-
     @Override
     public void initialize(Context pluginContext) {
         // Load/Reload laser blade json models
-        var modelManager = LaserBladeModelManager.getInstance();
-        modelManager.reload();
-        // Register UnbakedModels of laser blades
-        laserBladeModelLocations = getLaserBladeModelLocations();
-        pluginContext.modifyModelOnLoad().register(ModelModifier.OVERRIDE_PHASE, this::modifyModelOnLoad);
-    }
-
-    private List<ModelResourceLocation> getLaserBladeModelLocations() {
-        ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
-        return getLaserBladeModelLocations(config.getLaserBladeItemIDs());
-    }
-
-    private List<ModelResourceLocation> getLaserBladeModelLocations(List<ResourceLocation> laserBladeItemIds) {
-        return laserBladeItemIds.stream()
-                .map(this::addItemPrefix)
-                .map(ModelResourceLocation::inventory)
-                .toList();
-    }
-
-    private ResourceLocation addItemPrefix(ResourceLocation id) {
-        String path = id.getPath();
-
-        if (!path.startsWith("item/")) {
-            return ResourceLocation.fromNamespaceAndPath(id.getNamespace(), "item/" + path);
-        }
-
-        return id;
-    }
-
-    private UnbakedModel modifyModelOnLoad(UnbakedModel model, ModelModifier.OnLoad.Context context) {
-        var location = ModelResourceLocation.inventory(context.resourceId());
-
-        if (laserBladeModelLocations.contains(location)) {
-            return new LBSwordItemModel();
-        }
-
-        return model;
+        LaserBladeModelManager.getInstance().reload();
     }
 }
